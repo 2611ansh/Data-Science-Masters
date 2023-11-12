a8209e1e7e368579ec1e5c31aed03a922b208780
Ansh Raghuvanshi <121013240+2611ansh@users.noreply.github.com>
2023-11-13 01:16:52 +0530

    python

---
+++ 1. python/calling_function_outside_module/payment/payment_details.py
@@ -0,0 +1,11 @@
+def payment():
+    print("this is my payment file")
+
+import os ,sys
+from os.path import dirname, join, abspath
+
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
+
+from course import course_details
+course_details.course()
